6f202757b27bb34372c5b78f38fff184643bf5bb
RishabhKodes <rishabhbhandari6@gmail.com>
2019-12-05 23:08:32 +0530

    script serialized

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const firstTable=()=>{
 
         db.query(sql1 , (err, result)=>{
             if(err) reject(err);
-            resolve("DB1 Created")
+            resolve("User Table Created")
         })
 
     })
@@ -37,33 +37,34 @@ const secondTable=()=>{
         let sql2 = 'CREATE TABLE jobtable(id VARCHAR(40), description VARCHAR(255), maxwage int(2), durationto TIME(0), durationfrom TIME(0), status VARCHAR(255), PRIMARY KEY(id),timestamp TIMESTAMP,workerid VARCHAR(40), FOREIGN KEY (workerid) REFERENCES usertable(id),empid VARCHAR(40),FOREIGN KEY (empid) REFERENCES usertable(id))';
         db.query(sql2 , (err, result)=>{
             if(err) reject(err);
-            resolve("DB2 Created");
+            resolve("Job Table Created");
         })
-
     })
 } 
-
+//Third
 const thirdTable=()=>{
     return new Promise((resolve,reject)=>{
         let sql3 = 'CREATE TABLE bidtable(id VARCHAR(40),status VARCHAR(255),jobid VARCHAR(40), FOREIGN KEY (jobid) REFERENCES jobtable(id),bidderid VARCHAR(40),FOREIGN KEY (bidderid) REFERENCES usertable(id), duration VARCHAR(5),PRIMARY KEY(id))';
         db.query(sql3 , (err, result)=>{
             if(err) throw err;
-            resolve("DB3 Created");
+            resolve("Bid Table Created");
             // console.log(result);
         })
         })
 }
-    
+
 firstTable().then(res1=>{
     console.log(res1);
     secondTable().then(res2=>{
         console.log(res2)
         thirdTable().then(res3=>{
-            console.log("DB3 Created");
-        });
+            console.log("Bid Table Created");
+        }).catch((err)=>{
+            console.log("Error in Bid table.")
+        })
     }).catch((err)=>{
-        console.log("Error in second query.")
+        console.log("Error in Job table.")
     })
 }).catch(err=>{
-    console.log("Error in first query")
+    console.log("Error in User table.")
 })
